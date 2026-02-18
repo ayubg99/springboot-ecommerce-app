05fb5319ca464312245c6093b8620c5e4c837930
Ayoub Gharbi <ayoubg9919@gmail.com>
2026-02-18 07:43:13 +0100

    Post Mapping

--- src/main/java/com/embarkx/FirstSpring/HelloController.java
+++ src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -2,6 +2,8 @@ package com.embarkx.FirstSpring;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello " + name + "!";
     }
 }
